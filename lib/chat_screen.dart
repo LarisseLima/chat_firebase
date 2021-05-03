35801c0fa41e53d25dd9b159fad7727279d18995
Larisse Lima <larisse.lima@matilha.digital>
2021-05-03 20:38:39 -0300

    Add Image Picker

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:chat/text_composer.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -7,15 +9,22 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  void _sendMessage({String text}) async {}
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Hi'),
-        elevation: 0,
-      ),
-      body: TextComposer(_sendMessage),
-    );
+  void _sendMessage({String text, File imgFile}) async {
+    if (imgFile != null) {
+      StorageUploadTask task = FirebaseStorage.instance
+          .ref()
+          .child(user.uid + DateTime.now().millisecondsSinceEpoch.toString())
+          .putFile(imgFile);
+    }
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Hi'),
+          elevation: 0,
+        ),
+        body: TextComposer(_sendMessage),
+      );
+    }
   }
 }
